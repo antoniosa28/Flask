025b969764ffc9e8c6b785ab2d5839c2604ed203
Antonio Marcos Barbosa de Sá <antonio.sa@ufu.br>
2025-01-15 15:09:52 -0300

    Ajustes finais

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request
 import google.generativeai as genai
 
 # Chave API
-genai.configure(api_key="AIzaSyClYeI7WsOpD-2FCEanStmn2MV7S1TPBGM")
+genai.configure(api_key="AIzaSyDKieJO0iC8vndmITnCw3Yc5SIOXfZccu0")
 
-app = Flask(__name__) # objeto app que define as rotas e lida com requisições HTTP
+app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST']) # definicao de rota, aceita tanto GET quanto POST
+@app.route('/', methods=['GET', 'POST'])
 def chatbot():
     response_text = ""
     if request.method == 'POST':
-        language = request.form.get('language') # recupera o código submetido pelo usuário.
+        language = request.form.get('language')
         if not language:
             response_text = "Por favor, selecione uma linguagem."
         else:
@@ -23,8 +23,8 @@ def chatbot():
             print(texto04)
             if question.strip():
                 try:
-                    model = genai.GenerativeModel('gemini-pro') # O modelo gemini-pro é usado para processar a solicitação.
-                    response = model.generate_content(texto04) # A função retorna a análise gerada pelo modelo em resposta ao texto enviado.
+                    model = genai.GenerativeModel('gemini-pro') 
+                    response = model.generate_content(texto04)
                     response_text = response.text
                 except Exception as e:
                     response_text = f"Erro ao gerar resposta: {str(e)}"
